418c719ddb46f5b1ac1066aa39ca34d3f1ea662f
SayedaRehmat <bc190408541@vu.edu.pk>
2025-07-30 06:18:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from fpdf import FPDF
 
-BASE_API = "https://clinomics-ai.onrender.com"  # Your FastAPI URL
+BASE_API = "https://clinomicsai-pro.onrender.com"
 
 st.set_page_config(page_title="ClinOmics AI Pro", layout="wide")
 
